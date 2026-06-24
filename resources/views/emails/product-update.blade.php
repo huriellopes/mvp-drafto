c9f24325a9a8ccc180b38e181ef662240d06ad66
Huriel Lopes <huriellopes1996@gmail.com>
2026-06-24 19:35:36 -0300

    Feat: Adjustment product update.

--- resources/views/emails/product-update.blade.php
+++ resources/views/emails/product-update.blade.php
@@ -1,10 +1,10 @@
 @extends('emails.layouts.base')
 
 @section('content')
-    <h1 class="title">{{ $update->title }}</h1>
     <p class="text">Olá, {{ $user->name }}! Temos novidades no {{ config('app.name') }}:</p>
-    {{-- Conteúdo já sanitizado (Purifier) ao salvar o comunicado. --}}
+
     <div class="text">{!! $update->content !!}</div>
+
     <div class="button-container">
         <a href="{{ $ctaUrl }}" class="button">Conferir agora</a>
     </div>
